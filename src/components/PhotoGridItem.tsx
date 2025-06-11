197623d5403a00f1382f321e5426afd9f2ffa251
Santiago B. <santiago.balladares@outlook.com>
2025-06-11 11:10:48 +0200

    Add loading placeholder to grid items

--- src/components/PhotoGridItem.tsx
+++ src/components/PhotoGridItem.tsx
@@ -1,11 +1,13 @@
 import type { CSSProperties } from 'react'
-import { memo, useMemo } from 'react'
+import { memo, useCallback, useMemo, useState } from 'react'
 import { Link } from 'react-router'
 
 import type { GridItem } from '../types'
 
 const PhotoGridItem = memo(
 	({ alt, height, id, left, src, top, width }: GridItem) => {
+		const [imageLoaded, setImageLoaded] = useState(false)
+
 		const rootStyle = useMemo<CSSProperties>(
 			() => ({
 				height,
@@ -16,6 +18,10 @@ const PhotoGridItem = memo(
 			[height, left, top, width]
 		)
 
+		const onImageLoad = useCallback(() => {
+			setImageLoaded(true)
+		}, [])
+
 		return (
 			<div className='absolute' style={rootStyle}>
 				<Link to={`/photos/${id}`}>
@@ -23,8 +29,11 @@ const PhotoGridItem = memo(
 						alt={alt}
 						src={src}
 						className='block w-full h-auto object-cover'
+						style={{ display: imageLoaded ? 'block' : 'none' }}
+						onLoad={onImageLoad}
 					/>
 				</Link>
+				{!imageLoaded && <div className='loading-block' />}
 			</div>
 		)
 	}
